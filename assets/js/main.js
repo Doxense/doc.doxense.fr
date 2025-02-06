7d0e2bbf550b12c72f64f810c5903c5090b51a97
Céline Dauchy <celine.dauchy@doxense.com>
2025-02-06 16:13:38 +0100

    Mise à jour page Bienvenue

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 let blockNews = document.querySelector('.doc_news--ul');
 
 let arrayContent = [
+{
 	date: "06 février 2025",
 	link:"/fr/Watchdoc/Content/A_Getting_Started/Release_610.htm",
     content: "nouvelles versions Watchdoc et Console de Supervision 6.1.0.5150, ainsi que Watchdoc Print Client for Windows 7.0.4723"
