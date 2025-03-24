ad152cd96ef954d05eeda338080baefcabc375c8
Céline Dauchy <celine.dauchy@doxense.com>
2025-03-24 08:54:47 +0100

    Mise à jour page d'accueil

--- Watchdoc/Resources/Scripts/require.config.js
+++ Watchdoc/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638781699927203813'
+    urlArgs: 't=638784030873358296'
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@ let blockNews = document.querySelector('.doc_news--ul');
 
 let arrayContent = [
 {
+	date: "21 mars 2025",
+	link:"/Watchdoc/A_Getting_Started/Release_610.htm",
+	content: "nouvelles versions de Watchdoc et Console de Supervision 6.1.0.5194, ainsi que Watchdoc Print Client for Windows 7.0.4907"
+},	
+{
 	date: "06 mars 2025",
 	link:"/Watchdoc/A_Getting_Started/Release_610.htm",
 	content: "nouvelles versions de Watchdoc et Console de Supervision 6.1.0.5178, ainsi que Watchdoc Print Client for Windows 7.0.4863"
@@ -57,11 +62,6 @@ let arrayContent = [
 	date: "19 septembre 2024",
 	link:"/Watchdoc/A_Getting_Started/Release_610.htm",
     content: "nouvelles versions Watchdoc et Console de Supervision 6.1.0.5025, ainsi que Watchdoc Print Client for Windows 7.0.4499"
-},
-{	
-	date: "06 septembre 2024",
-	link:"/Watchdoc/A_Getting_Started/Release_610.htm",
-    content: "nouvelles versions Watchdoc et Console de Supervision 6.1.0.4996, ainsi que Watchdoc Print Client for Windows 7.0.4458"
 },
 ]
 
