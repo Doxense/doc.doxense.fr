2b9a8b0ab024025005baec65f5482c62f851daec
Céline Dauchy <celine.dauchy@doxense.com>
2025-09-04 11:05:06 +0200

    Mise à jour version et note de version

--- Watchdoc/Resources/Scripts/require.config.js
+++ Watchdoc/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638924331083277208'
+    urlArgs: 't=638925801950619997'
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@ let blockNews = document.querySelector('.doc_news--ul');
 
 let arrayContent = [
 {	
+	date: "04 septembre 2025",
+	link: "/Watchdoc/A_Getting_Started/Release_611.htm",
+	content: "nouvelles versions Watchdoc, Console de Supervision 6.1.1.5360 et Watchdoc Print Client for Windows 7.0.5496"
+},
+{	
 	date: "02 septembre 2025",
 	link: "/Focalist/D_NotesVersions/Notes_Versions.htm",
 	content: "nouvelle version Focalist 9.34.0"
