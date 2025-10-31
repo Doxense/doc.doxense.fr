690939c21e4f9e714f513c3a149e8c2615a55ae4
Céline Dauchy <celine.dauchy@doxense.com>
2025-10-31 09:53:30 +0100

    Mise à jour page d'accueil

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ let blockNews = document.querySelector('.doc_news--ul');
 
 let arrayContent = [
 {	
+	date: "31 octobre 2025",
+	link: "/Watchdoc/A_Getting_Started/Release_611.htm",
+	content: "nouvelles versions Watchdoc, Console de Supervision 6.1.1.5417 et Watchdoc Print Client for Windows 7.0.5821"
+{	
 	date: "20 octobre 2025",
 	link: "/Focalist/D_NotesVersions/Notes_Versions.htm",
 	content: "nouvelle version Focalist 9.36.0"
