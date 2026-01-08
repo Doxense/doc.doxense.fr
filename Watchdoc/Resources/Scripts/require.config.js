942d462032bf36137a4bc658bcde1838cb1362c8
Céline Dauchy <celine.dauchy@doxense.com>
2026-01-08 17:20:32 +0100

    Mise à jour nouvelle version WES Ricoh - nouveaux jetons d'authent.

--- Watchdoc/Resources/Scripts/require.config.js
+++ Watchdoc/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=639034009750671903'
+    urlArgs: 't=639034895382149930'
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,14 @@ let blockNews = document.querySelector('.doc_news--ul');
 
 let arrayContent = [
 {
+	date: "08 janvier 2026",
+	link: "/Watchdoc/A_Getting_Started/Release_611.htm",
+	content: "nouvelles versions de Watchdoc, Console de Supervision 6.1.1.5487, Watchdoc Print Client for Windows 7.0.5944 et Skyprint 7.0.5947"
+},
+{
 	date: "07 janvier 2026",
 	link: "/Watchdoc/A_Getting_Started/Release_611.htm",
-	content: "nouvelles versions de Watchdoc, supervision Console 6.1.1.5486, Watchdoc Print Client for Windows 7.0.5944 et Skyprint 7.0.5947"
+	content: "nouvelles versions de Watchdoc, Console de Supervision 6.1.1.5486, Watchdoc Print Client for Windows 7.0.5944 et Skyprint 7.0.5947"
 },
 {	
 	date: "19 décembre 2025",
@@ -16,7 +21,7 @@ let arrayContent = [
 {
 	date: "11 décembre 2025",
 	link: "/Watchdoc/A_Getting_Started/Release_611.htm",
-	content: "nouvelles versions de Watchdoc, supervision Console 6.1.1.5458, Watchdoc Print Client for Windows 7.0.5912 et Skyprint 7.0.5911 "
+	content: "nouvelles versions de Watchdoc, Console de Supervision 6.1.1.5458, Watchdoc Print Client for Windows 7.0.5912 et Skyprint 7.0.5911 "
 },
 {	
 	date: "04 décembre 2025",
